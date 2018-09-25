de720adb62fd71c1fd29409fb55c2ba36e6cf66c
Arne von Appen <arne.appen@limetec-biotechnologies.de>
2018-09-25 14:37:58 +0200

    add output message

--- Loader/YamlLoader.php
+++ Loader/YamlLoader.php
@@ -105,6 +105,7 @@ class YamlLoader
         $this->loadFixtureFiles();
         foreach ($this->fixture_files as $file) {
             $fixture_data = Yaml::parse(file_get_contents($file));
+            echo $file;
             // if nothing is specified, we use doctrine orm for persistence
             $persistence = isset($fixture_data['persistence']) ? $fixture_data['persistence'] : 'orm';
 
